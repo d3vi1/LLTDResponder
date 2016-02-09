59b90c2c1d79e0739969e10188947bf534b4ee44
Răzvan Corneliu C.R. VILT <razvan.vilt@me.com>
2016-02-09 21:51:36 +0200

    Added a Linux/ESXi skeleton. It assumes Posix Threads and uses Linux netlink APIs for detecting interfaces. Alternatively, it could use dbus for interfaces, but this will require autoconf.

---
+++ LLMNRd/linux-main.c
@@ -0,0 +1,9 @@
+//
+//  linux-main.c
+//  lltdDaemon
+//
+//  Created by Răzvan Corneliu C.R. VILT on 09.02.2016.
+//  Copyright © 2016 Răzvan Corneliu C.R. VILT. All rights reserved.
+//
+
+#include "linux-main.h"
---
+++ LLMNRd/linux-main.h
@@ -0,0 +1,14 @@
+//
+//  linux-main.h
+//  lltdDaemon
+//
+//  Created by Răzvan Corneliu C.R. VILT on 09.02.2016.
+//  Copyright © 2016 Răzvan Corneliu C.R. VILT. All rights reserved.
+//
+
+#ifndef linux_main_h
+#define linux_main_h
+
+#include <stdio.h>
+
+#endif /* linux_main_h */
