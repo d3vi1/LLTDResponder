5da514266718595b9048dfee2592bce238de9324
Răzvan Corneliu C.R. VILT <razvan.vilt@me.com>
2016-03-02 18:50:40 +0200

    Added some more Linux functionality It currently builds.

---
+++ lltdDaemon/linux-ops.h
@@ -0,0 +1,28 @@
+/******************************************************************************
+ *                                                                            *
+ *   linux-ops.c                                                              *
+ *   lltdDaemon                                                               *
+ *                                                                            *
+ *   Created by Răzvan Corneliu C.R. VILT on 02.03.2016.                      *
+ *   Copyright © 2014 Răzvan Corneliu C.R. VILT. All rights reserved.         *
+ *                                                                            *
+ ******************************************************************************/
+
+#ifndef LLTDd_linux_ops_h
+#define LLTDd_linux_ops_h
+
+void getMachineName                (char **data, size_t *stringSize);
+void getFriendlyName               (char **data, size_t *stringSize);
+void getSupportInfo                (void **data, size_t *stringSize);
+void getIconImage                  (void **icon, size_t *iconsize);
+void getUpnpUuid                   (void **data);
+void getHwId                       (void *data);
+void getDetailedIconImage          (void **data, size_t *iconsize);// Only with QueryLargeTLV
+void getHostCharacteristics        (void *data);
+void getComponentTable             (void **data, size_t *dataSize);// Only with QueryLargeTLV
+void getPerformanceCounterFrequency(void *data);
+void setPromiscuous                (void *currentNetworkInterface, _Bool set);
+_Bool getWifiMode                  (void *currentNetworkInterface);
+_Bool getBSSID                     (void **data, void *currentNetworkInterface);
+
+#endif
