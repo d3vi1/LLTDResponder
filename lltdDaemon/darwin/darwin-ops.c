30a7c0ddb3f08b1fdf9c45efa40333b99108358f
Răzvan Corneliu C.R. VILT <16434603+d3vi1@users.noreply.github.com>
2025-12-31 17:50:10 +0200

    Fix macOS UTType includes and set TERM for Watcom

--- lltdDaemon/darwin/darwin-ops.c
+++ lltdDaemon/darwin/darwin-ops.c
@@ -11,7 +11,11 @@
 #include "../lltdDaemon.h"
 
 #if defined(__APPLE__) && defined(__has_include)
-#if __has_include(<CoreServices/UTType.h>)
+#if __has_include(<UniformTypeIdentifiers/UTType.h>)
+#include <UniformTypeIdentifiers/UTType.h>
+#elif __has_include(<MobileCoreServices/UTType.h>)
+#include <MobileCoreServices/UTType.h>
+#elif __has_include(<CoreServices/UTType.h>)
 #include <CoreServices/UTType.h>
 #endif
 #endif
